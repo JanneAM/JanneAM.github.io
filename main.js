a4781504a78598db40111f40b90243afc8310986
JanneAM <janne.majasalmi@gmail.com>
2020-10-04 13:33:50 +0300

    sequencing animations ready, needs finetuning still

--- main.js
+++ main.js
@@ -4,60 +4,115 @@ const gallery = document.getElementById("gallery");
 const contact = document.getElementById("contact");
 
 let activePage;
+let resetElement;
 
-const tl = gsap.timeline();
-
-//fadeout function for outros for all elements
-function fadeOut(element) {
-  tl.fromTo(element, 3, { opacity: 1 }, { opacity: 0 });
-}
+let masterTl = gsap.timeline();
 
 //animate different pages in different manner
 function animateHome() {
-  tl.set(home, { zIndex: 1 }, 0.25).fromTo(
+  const tl = gsap.timeline();
+
+  tl.set(home, { zIndex: 1, opacity: 1 }, 1).fromTo(
     home,
     1,
     { y: "-100%" },
     { y: "0%", ease: Power2.easeInOut }
   );
   activePage = home;
+
+  return tl;
 }
 
 function animateExperience() {
-  tl.set(experience, { zIndex: 3 }, 0.25).fromTo(
+  const tl = gsap.timeline();
+
+  tl.set(experience, { zIndex: 1, opacity: 1 }, 1).fromTo(
     experience,
     1,
-    { x: "-100%" },
-    { x: "0%", ease: Power2.easeInOut },
-    1
+    { y: "-100%" },
+    { y: "0%", ease: Power2.easeInOut }
   );
+  activePage = experience;
+
+  return tl;
 }
 
 function animateGallery() {
-  tl.fromTo(gallery, 1, { y: "-100%" }, { y: "0%", ease: Power2.easeInOut });
+  const tl = gsap.timeline();
+
+  tl.set(gallery, { zIndex: 1, opacity: 1 }, 1).fromTo(
+    gallery,
+    1,
+    { y: "-100%" },
+    { y: "0%", ease: Power2.easeInOut }
+  );
+  activePage = gallery;
+
+  return tl;
 }
 
 function animateContact() {
-  tl.fromTo(contact, 1, { y: "0%" }, { y: "100%", ease: Power2.easeInOut });
+  const tl = gsap.timeline();
+
+  tl.set(contact, { zIndex: 1, opacity: 1 }, 1).fromTo(
+    contact,
+    1,
+    { y: "-100%" },
+    { y: "0%", ease: Power2.easeInOut }
+  );
+  activePage = contact;
+
+  return tl;
+}
+
+//fadeout function for outros for all elements
+function animateOut(element) {
+  const tl = gsap.timeline();
+  const content = element.querySelector(".content");
+
+  tl.fromTo(content, 1, { opacity: 1 }, { opacity: 0 });
+  resetElement = element;
+  return tl;
+}
+
+//reset element arrtibutes for future animations
+function resetElem(element) {
+  const tl = gsap.timeline();
+  const content = element.querySelector(".content");
+
+  tl.set(element, { zIndex: 0 }).set(content, { opacity: 1 });
+
+  return tl;
 }
 
 //Fire functions with key presses:
 window.addEventListener("keydown", (event) => {
-  if (tl.isActive()) return;
+  if (masterTl.isActive()) return;
 
   switch (event.key) {
     case "ArrowUp":
-      fadeOut(activePage);
-      animateHome();
+      masterTl
+        .add(animateOut(activePage))
+        .add(animateHome)
+        .add(resetElem(resetElement), "+=1");
       break;
     case "ArrowLeft":
-      animateExperience();
+      masterTl
+        .add(animateOut(activePage))
+        .add(animateExperience)
+        .add(resetElem(resetElement), "+=1");
       break;
     case "ArrowRight":
-      animateGallery();
+      masterTl
+        .add(animateOut(activePage))
+        .add(animateGallery)
+        .add(resetElem(resetElement), "+=1");
       break;
     case "ArrowDown":
-      animateContact();
+      masterTl
+        .add(animateOut(activePage))
+        .add(animateContact)
+        .add(resetElem(resetElement), "+=1");
       break;
   }
 });
